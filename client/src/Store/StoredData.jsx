bc04be72dd15ca97ba350eca086136fc2557cfb9
Sudharshan Rao Nellore <134708310+Sudharshannellore@users.noreply.github.com>
2025-05-02 11:34:02 +0530

    Update StoredData.jsx

--- client/src/Store/StoredData.jsx
+++ client/src/Store/StoredData.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // Categories Data
 export const getCategoriesData = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/admin/get/category');
+    const response = await axios.get('https://localbazaar.onrender.com/get/category');
     console.log("Fetched Categories:", response.data);  // Debug log
     return response.data;
   } catch (error) {
@@ -15,7 +15,7 @@ export const getCategoriesData = async () => {
 // Vendors Data
 export const getVendorsData = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/admin/get/vendor-data');
+    const response = await axios.get('https://localbazaar.onrender.com/admin/get/vendor-data');
     console.log("Fetched Categories:", response.data);  // Debug log
     return response.data;
   } catch (error) {
